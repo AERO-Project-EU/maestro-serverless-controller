c67f4b1933be027129a0ceb7b1b89c163ea2a30f
vmatsoukas <vmatsoukas@ubitech.eu>
2024-06-13 15:39:03 +0300

    feat: maestro rest client to communicate with maestro backend

---
+++ src/main/java/eu/ubitech/client/MaestroRestClient.java
@@ -0,0 +1,34 @@
+package eu.ubitech.client;
+
+import eu.ubitech.transfer.MaestroAuthDto;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/api/v1")
+@RegisterClientHeaders
+@RegisterRestClient(configKey = "maestro-rest-api")
+public interface MaestroRestClient {
+
+    @POST
+    @Path("/auth/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response maestroAuthenticate(@RequestBody MaestroAuthDto maestroAuthDto);
+
+    @POST
+    @Path("/applicationinstance/{applicationInstanceID}/request/deployment")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response requestDeployment(@CookieParam("auth_token") String authToken, @PathParam("applicationInstanceID") Long applicationInstanceID);
+
+    @POST
+    @Path("/applicationinstance/{applicationInstanceID}/request/undeployment")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response requestUndeployment(@CookieParam("auth_token") String authToken, @PathParam("applicationInstanceID") Long applicationInstanceID);
+
+}
